a35a2f2ab5cf37fdc928d62ea9489e2584e4f6d1
mocha <mocha.gamedev@gmail.com>
2026-01-30 03:33:28 -0500

    organization, fixing puzzle class, adding back button to puzzles, adding interaction blocker when popup from any of the menu bar

--- Assets/Scripts/EscapeRoom/Shared/Logic/MenuBarScript.cs
+++ Assets/Scripts/EscapeRoom/Shared/Logic/MenuBarScript.cs
@@ -5,11 +5,7 @@ public class MenuBarScript : MonoBehaviour
 {
     [SerializeField] private string currentMenuName = "none";
     private GameObject currentPopUp;
-    private GameObject menuBarButtons;
-    private Button logButton;
-    private Button moveButton;
-    private Button itemButton;
-    private Button infoButton;
+    private GameObject interactionBlocker;
     private GameObject popUpAreas;
     private GameObject logPopUp;
     private GameObject movePopUp;
@@ -17,23 +13,15 @@ public class MenuBarScript : MonoBehaviour
     private GameObject infoPopUp;
 
     void Start()
-    {
-        menuBarButtons = transform.Find("MenuBarButtons").gameObject;
-        logButton = menuBarButtons.transform.Find("LogButton").GetComponent<Button>();
-        moveButton = menuBarButtons.transform.Find("MoveButton").GetComponent<Button>();
-        itemButton = menuBarButtons.transform.Find("ItemButton").GetComponent<Button>();
-        infoButton = menuBarButtons.transform.Find("InfoButton").GetComponent<Button>();
-    
+    {    
+        interactionBlocker = transform.Find("InteractionBlocker").gameObject;
+        interactionBlocker.SetActive(false);
+
         popUpAreas = transform.Find("PopUpAreas").gameObject;
         logPopUp = popUpAreas.transform.Find("LogPopUp").gameObject;
         movePopUp = popUpAreas.transform.Find("MovePopUp").gameObject;
         itemPopUp = popUpAreas.transform.Find("ItemPopUp").gameObject;
         infoPopUp = popUpAreas.transform.Find("InfoPopUp").gameObject;
-    }
-
-    void Update()
-    {
-        
     }
 
     public void ChangeState(string newMenuName)
@@ -67,6 +55,7 @@ public class MenuBarScript : MonoBehaviour
         currentMenuName = "none";
         currentPopUp.SetActive(false);
         currentPopUp = null;
+        interactionBlocker.SetActive(false);
     }
 
     private void ShowPopUp(GameObject popUp, string newMenuName)
@@ -75,5 +64,6 @@ public class MenuBarScript : MonoBehaviour
         currentPopUp?.SetActive(false);
         popUp.SetActive(true);
         currentPopUp = popUp;
+        interactionBlocker.SetActive(true);
     }
 }
--- Assets/Scripts/EscapeRoom/Shared/Logic/Puzzle.cs
+++ Assets/Scripts/EscapeRoom/Shared/Logic/Puzzle.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Puzzle : MonoBehaviour
 {
@@ -12,5 +13,24 @@ public class Puzzle : MonoBehaviour
         // print($"the answer is {new string(answer)} and the guess was {new string(guess)}");
         // TODO: VERY TEMPORARY
         if (answer.SequenceEqual(guess)) GameObject.Find("DemoMessage").GetComponent<TextMeshProUGUI>().text = "DEMO MESSAGE: SUCCESS";
+    }
+
+    public void IncrementDigit()
+    {
+        GameObject digit = EventSystem.current.currentSelectedGameObject;
+        TextMeshProUGUI digitTMP = digit.GetComponentInChildren<TextMeshProUGUI>();
+        int currentDigit = int.Parse(digitTMP.text);
+
+        if (currentDigit == 9)
+        {
+            currentDigit = 0;
+        }
+        else
+        {
+            currentDigit++;
+        }
+
+        digitTMP.text = currentDigit.ToString();
+        guess[digit.transform.GetSiblingIndex()] = (char)('0' + currentDigit);
     }
 }
