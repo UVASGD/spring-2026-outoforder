1fe382baa2658846aadb8e9cc1eebd1d31b451fd
Nova <nova.blanco001@gmail.com>
2026-01-30 18:38:41 -0500

    created basic functionality for locked drawer

---
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/LockedDrawerPuzzle.cs
@@ -0,0 +1,8 @@
+public class LockedDrawerPuzzle : Puzzle
+{
+    void Awake()
+    {
+        answer = new char[] { '2', '3', '5', '7' };
+        guess = new char[] { '0', '0', '0', '0' };
+    }
+}
