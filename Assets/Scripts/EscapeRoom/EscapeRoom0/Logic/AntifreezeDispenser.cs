ea3981942e89b2fa8a5abc828c09d57cd7e14592
Aidan Liu <boomcreeper296@gmail.com>
2026-02-01 11:27:31 -0500

    Add node structure

--- Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/AntifreezeDispenser.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/AntifreezeDispenser.cs
@@ -2,10 +2,28 @@
 {
     public class AntifreezeDispenser : Puzzle
     {
+        private enum State
+        {
+            Idle, LetterSelected
+        }
+
+        private State currentState = State.Idle;
+        
         void Awake()
         {
             guess = new char[] { 'F', 'A', 'R', 'E', 'Z', 'E', 'N', 'I', 'E', 'T'};
             answer = new char[] { 'A', 'N', 'T', 'I', 'F', 'R', 'E', 'E', 'Z', 'E'};
+        }
+
+        void OnLetterClick()
+        {
+            // If Idle: Highlight letter
+            // If LetterSelected: Swap letters
+        }
+
+        protected override void SolvedPuzzleSpecific()
+        {
+            
         }
     }
 }
