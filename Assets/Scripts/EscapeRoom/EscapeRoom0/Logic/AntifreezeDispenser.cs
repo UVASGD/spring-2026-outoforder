8c20b1a2898451e26cc24392c1e6d4f20e3fa9f1
Aidan Liu <boomcreeper296@gmail.com>
2026-02-01 11:02:33 -0500

    Initial puzzle class

---
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/AntifreezeDispenser.cs
@@ -0,0 +1,11 @@
+﻿namespace EscapeRoom.EscapeRoom0.Logic
+{
+    public class AntifreezeDispenser : Puzzle
+    {
+        void Awake()
+        {
+            guess = new char[] { 'F', 'A', 'R', 'E', 'Z', 'E', 'N', 'I', 'E', 'T'};
+            answer = new char[] { 'A', 'N', 'T', 'I', 'F', 'R', 'E', 'E', 'Z', 'E'};
+        }
+    }
+}
