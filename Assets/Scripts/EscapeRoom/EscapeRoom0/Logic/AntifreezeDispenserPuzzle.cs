91835e723610607940bcee6a763bd2d8a3774cde
Aidan Liu <boomcreeper296@gmail.com>
2026-02-04 18:35:56 -0500

    Letter swapping

--- Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/AntifreezeDispenserPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/AntifreezeDispenserPuzzle.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace EscapeRoom.EscapeRoom0.Logic
 {
@@ -17,18 +19,47 @@ namespace EscapeRoom.EscapeRoom0.Logic
         {
             guess = new char[] { 'F', 'A', 'R', 'E', 'Z', 'E', 'N', 'I', 'E', 'T'};
             answer = new char[] { 'A', 'N', 'T', 'I', 'F', 'R', 'E', 'E', 'Z', 'E'};
+            
+            // Initialize button texts
+            for (int i = 0; i < guess.Length; i++)
+            {
+                TextMeshProUGUI text = transform.GetChild(i).GetComponentInChildren<TextMeshProUGUI>();
+                text.text = guess[i].ToString();
+            }
         }
 
         public void OnLetterClick()
         {
+            // If Idle: Highlight letter
+            // If LetterSelected: Swap letters
             if (currentState == State.Idle)
             {
                 GameObject button = EventSystem.current.currentSelectedGameObject;
                 selectedLetterIndex = button.transform.GetSiblingIndex();
-                Debug.Log(selectedLetterIndex);
+                button.GetComponent<Image>().color = Color.yellow;
+                currentState = State.LetterSelected;
             }
-            // If Idle: Highlight letter
-            // If LetterSelected: Swap letters
+            else
+            {
+                GameObject newButton = EventSystem.current.currentSelectedGameObject;
+                int newLetterIndex = newButton.transform.GetSiblingIndex();
+                
+                // Swap letters in guess
+                (guess[selectedLetterIndex], guess[newLetterIndex]) = (guess[newLetterIndex], guess[selectedLetterIndex]);
+                
+                
+                // Update button texts
+                TextMeshProUGUI selectedLetterTMP = transform.GetChild(selectedLetterIndex).GetComponentInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI newLetterTMP = newButton.GetComponentInChildren<TextMeshProUGUI>();
+                selectedLetterTMP.text = guess[selectedLetterIndex].ToString();
+                newLetterTMP.text = guess[newLetterIndex].ToString();
+                
+                // Unhighlight previously selected letter
+                GameObject previousButton = transform.GetChild(selectedLetterIndex).gameObject;
+                previousButton.GetComponent<Image>().color = Color.white;
+
+                currentState = State.Idle;
+            }
         }
 
         protected override void SolvedPuzzleSpecific()
