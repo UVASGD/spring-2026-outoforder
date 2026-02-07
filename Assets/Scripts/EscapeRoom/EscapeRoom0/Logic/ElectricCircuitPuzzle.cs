2b8bf77a72e34f21de61833fd72b1c6cc3ed64a6
Mira Khan <115938924+m1ra-k@users.noreply.github.com>
2026-02-07 12:03:56 -0500

    emma-escaperoom0-puzzles-0 (#6)
    * json meta suggestions
    
    * Pls check mira
    
    idk why its not working, commented the line its getting stuck on
    
    * slot selection bug fix and cleaning code
    
    ---------
    
    Co-authored-by: mayoeh <mayoeh43@gmail.com>
    Co-authored-by: mocha <mocha.gamedev@gmail.com>

---
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/ElectricCircuitPuzzle.cs
@@ -0,0 +1,119 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ElectricCircuitPuzzle : Puzzle
+{
+    private TMP_InputField inputField;
+    private Image finalLight;
+    private Button firstSlot;
+    private Button secondSlot;
+    private Button currentSlot;
+    private char? firstGateCode = null;
+    private char? secondGateCode = null;
+    private Dictionary<string, char> firstSlotValidGate = new Dictionary<string, char>
+    {
+        { "AND",  '0' },
+        { "OR",   '1' },
+        { "NAND", '2' },
+        { "NOR",  '3' },
+        { "XOR",  '4' },
+        { "XNOR", '5' }
+    };
+    private Dictionary<string, char> secondSlotValidGate = new Dictionary<string, char>
+    {
+        { "NOT", '6' }
+    };
+
+    void Awake()
+    {
+        inputField = GameObject.Find("InputField").gameObject.GetComponent<TMP_InputField>();
+        finalLight = GameObject.Find("FinalLight").gameObject.GetComponent<Image>();
+        firstSlot = GameObject.Find("FirstSlot").gameObject.GetComponent<Button>();
+        secondSlot = GameObject.Find("SecondSlot").gameObject.GetComponent<Button>();
+
+        inputField.gameObject.SetActive(false);
+        inputField.onSubmit.AddListener(OnTextEntered);
+        finalLight.enabled = false;
+    }
+
+    public void OnFirstSlotClicked()
+    {
+        OpenInputField(firstSlot);
+        print($"First slot was clicked");
+    }
+
+    public void OnSecondSlotClicked()
+    {
+        OpenInputField(secondSlot);
+        print("Second slot was clicked");
+    }
+
+    private void OpenInputField(Button slotButton)
+    {
+        currentSlot = slotButton;
+        inputField.text = "";
+        inputField.gameObject.SetActive(true);
+        inputField.ActivateInputField();
+        inputField.placeholder.GetComponent<TMP_Text>().text = "LOGIC GATE TYPE (BASIC ONLY)";
+    }
+
+    private void OnTextEntered(string text)
+    {
+        text = text.Trim().ToUpper();
+        print("Input " + text);
+
+        if (!((currentSlot == firstSlot) ? firstSlotValidGate : secondSlotValidGate).ContainsKey(text))
+        {
+            print("TODO: ERROR UI");
+            inputField.text = "";
+            return;
+        }
+        if (currentSlot == firstSlot) firstGateCode = firstSlotValidGate[text]; else secondGateCode = secondSlotValidGate[text];
+
+        inputField.gameObject.SetActive(false);
+        currentSlot = null;
+
+        EvaluateCircuit();
+    }
+
+
+    private void EvaluateCircuit()
+    {
+        print("evaluating");
+        
+        if (firstGateCode == null || secondGateCode == null)
+        {
+            print("One or more gates aren't set");
+            return;
+        }
+
+        bool firstResult = firstGate(firstGateCode.Value, true, true);
+        bool finalResult = secondGate(secondGateCode.Value, firstResult);
+
+        print($"First gate {firstResult}, Final result {finalResult}");
+
+        finalLight.enabled = finalResult;
+    }
+
+
+    private bool firstGate(char gate, bool a, bool b)
+    {
+        return gate switch
+        {
+            '0' => a & b,
+            '1' => a | b,
+            '2' => !(a & b),
+            '3' => !(a | b),
+            '4' => a ^ b,
+            '5' => !(a ^ b),
+            _ => false
+        };
+    }
+
+    private bool secondGate(char gate, bool a)
+    {
+        return gate == '6' ? !a : false;
+    }
+}
