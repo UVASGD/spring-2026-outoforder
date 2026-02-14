3b5eb5aba92fd079d2033e09ba5e97bb6c1db314
Aidan Liu <boomcreeper296@gmail.com>
2026-02-13 20:02:26 -0500

    laptop puzzle w/ textbar

---
+++ Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/LaptopPasswordPuzzle.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace EscapeRoom.EscapeRoom1.Logic
+{
+    public class LaptopPasswordPuzzle : Puzzle
+    {
+        [SerializeField] TMP_InputField inputField;
+        void Awake()
+        {
+            answer = new char[] { '4', '2', '1', '0' };
+        }
+
+        public void OnEdit()
+        {
+            guess = inputField.text.ToCharArray();
+        }
+        
+        
+    }
+}
