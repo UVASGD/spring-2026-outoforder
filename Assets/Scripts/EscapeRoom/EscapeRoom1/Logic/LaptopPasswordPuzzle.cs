0e8397c01c9fdb468e3484cdc5b98646bb88d092
Aidan Liu <boomcreeper296@gmail.com>
2026-02-13 20:23:42 -0500

    Keypad to keep it point & click

--- Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/LaptopPasswordPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/LaptopPasswordPuzzle.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace EscapeRoom.EscapeRoom1.Logic
 {
@@ -8,11 +9,24 @@ namespace EscapeRoom.EscapeRoom1.Logic
         [SerializeField] TMP_InputField inputField;
         void Awake()
         {
-            answer = new char[] { '4', '2', '1', '0' };
+            answer = new char[] { '4', '2', '1', '2' };
         }
 
-        public void OnEdit()
+        public void OnKeypadClick()
         {
+            var button = EventSystem.current.currentSelectedGameObject; 
+            var num = button.transform.GetSiblingIndex() + 1;
+            if (num == 10)
+            {
+                if (inputField.text.Length > 0)
+                // Remove last character
+                inputField.text = inputField.text[..^1];
+            }
+            else
+            {
+                inputField.text += num.ToString();
+            }
+
             guess = inputField.text.ToCharArray();
         }
         
