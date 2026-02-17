42c906f6af8077478125e7e74e2220820d02fe75
mocha <mocha.gamedev@gmail.com>
2026-02-17 03:31:14 -0500

    fixed monitor not turning on bug

--- Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/Monitors.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/Monitors.cs
@@ -7,9 +7,8 @@ public class Monitors : MonoBehaviour
 
     void Update()
     {
-        if (GameData.escapeRoomGameplayManagerScript.interactingWith.Equals("Monitors") && !monitorsOn && GameProgression.GameProgressionInstance.GetFlag("firstInteractionRemote") && GameProgression.GameProgressionInstance.GetFlag("firstInteractionMonitors"))
+        if (!monitorsOn && GameProgression.GameProgressionInstance.GetFlag("usedRemote"))
         {
-            print("YAYYYY");
             monitorsOn = true;
             GameData.escapeRoomGameplayManagerScript.locations[4].GetComponent<Image>().sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["Location4Secondary"];
         }
