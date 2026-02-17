c9e4c6aed289ca441aff34437307350df0940f00
AstralPhoenix01 <cardinalphoenix940@gmail.com>
2026-02-17 14:33:16 -0500

    Copied code from 'LightSwitchPuzzle' script into 'ConveyorBeltPuzzle' script

--- Assets/Scripts/EscapeRoom/EscapeRoom2/Logic/ConveyorBeltPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom2/Logic/ConveyorBeltPuzzle.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ConveyorBeltPuzzle : MonoBehaviour
+public class ConveyorBeltPuzzle : Puzzle
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject submit;
+    private bool leverRepaired;
+
+    void Awake()
     {
-        
+        answer = new char[] { '9', '0', '6', '0', '1', '2', '0' };
+        guess = new char[] { '0', '0', '0', '0', '0', '0', '0' };
+
+        submit = transform.Find("Submit").gameObject;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!leverRepaired && GameProgression.GameProgressionInstance.GetFlag("usedLever"))
+        {
+            leverRepaired = true;
+            submit.GetComponent<Button>().enabled = true;
+            submit.GetComponent<Image>().enabled = true;
+        }
+    }
+
+    protected override void SolvedPuzzleSpecific()
+    {
+        GameData.escapeRoomGameplayManagerScript.locations.ForEach(location => location.GetComponent<Image>().color = Color.white);
+
+        gameObject.GetComponent<Image>().sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["UnlockedLightSwitchPuzzle"];
+
+        GameProgression.GameProgressionInstance.SetFlag("firstInteractionLightSwitchPuzzle", true);
+        GameProgression.GameProgressionInstance.SetFlag("solvedLightSwitchPuzzle", true);
+        gameObject.GetComponent<ManualInteraction>().ItemInteraction();
+        GameProgression.GameProgressionInstance.SetFlag("lastInteractionLightSwitchPuzzle", true);
     }
 }
