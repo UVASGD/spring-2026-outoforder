6f1167966310bb24a81644a3c600ef10910ba22c
mayoeh <mayoeh43@gmail.com>
2026-03-02 00:06:20 -0500

    Script Removal
    Removed unnecessary script & renamed working script

--- Assets/Scripts/EscapeRoom/EscapeRoom2/Logic/FreezeSettingPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom2/Logic/FreezeSettingPuzzle.cs
@@ -1,16 +1,77 @@
+using System.Collections; // IEnumerator
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq; // Lets solve function access answer & guess outside of awake
 
-public class FreezeSettingScript : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+public class FreezeSettingPuzzle : Puzzle
+{    public GameObject submit;
+    private bool leverRepaired;
+
+    public GameObject slider;
+
+    public GameObject errorText;
+    public float errorDuration = 2f;
+
+    void Awake()
     {
-        
+        answer = new char[] { '1', '5' };
+        guess = new char[] { '0', '0' };
+
+        submit = transform.Find("SubmitButton").gameObject;
+        slider.SetActive(false);
+        errorText.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!leverRepaired && GameProgression.GameProgressionInstance.GetFlag("usedLever"))
+        {
+            leverRepaired = true;
+            submit.GetComponent<Button>().enabled = true;
+            submit.GetComponent<Image>().enabled = true;
+        }
+    }
+
+    protected override void SolvedPuzzleSpecific()
+    {
+        //GameData.escapeRoomGameplayManagerScript.locations.ForEach(location => location.GetComponent<Image>().color = Color.white);
+
+        gameObject.GetComponent<Image>().sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["UnlockedFreezeSettingPuzzle"];
+
+        GameProgression.GameProgressionInstance.SetFlag("firstInteractionFreezeSettingPuzzle", true);
+        GameProgression.GameProgressionInstance.SetFlag("solvedFreezeSettingPuzzle", true);
+        gameObject.GetComponent<ManualInteraction>().ItemInteraction();
+        GameProgression.GameProgressionInstance.SetFlag("lastInteractionFreezeSettingPuzzle", true);
+    }
+    
+    public void spawnSlider()
+    {
+        if (slider.activeInHierarchy)
+        {
+            slider.SetActive(false);
+        } else
+        {
+            slider.SetActive(true);
+        }
+    }
+
+    public void SolvedFeedback()
+    {
+        if (!solved && answer.SequenceEqual(guess))
+        {
+            Debug.Log("Correct Answer");
+        }
+        else
+        {
+            StartCoroutine(ShowErrorMessage());
+        }
+    }
+
+    private IEnumerator ShowErrorMessage()
+    {
+        errorText.SetActive(true);
+        yield return new WaitForSeconds(errorDuration);
+        errorText.SetActive(false);
     }
 }
