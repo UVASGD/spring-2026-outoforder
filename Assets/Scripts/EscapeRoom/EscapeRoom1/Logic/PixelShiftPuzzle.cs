b4daf5fd061c7e568e4912257f8e1124e9781133
mocha <mocha.gamedev@gmail.com>
2026-03-09 16:30:13 -0400

    starting swap logic

--- Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/PixelShiftPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/PixelShiftPuzzle.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PixelShiftPuzzle : Puzzle
 {
+    private GameObject pixelA;
+    private int indexA;
+    private GameObject pixelB;
+    private int indexB;
+
     void Awake()
     {
         answer = new char[] 
@@ -21,5 +26,26 @@ public class PixelShiftPuzzle : Puzzle
     void Update()
     {
         
+    }
+
+    public void SwapPixel()
+    {
+        if (pixelA == null)
+        {
+            pixelA = gameObject;
+            indexA = pixelA.transform.GetSiblingIndex();
+        }
+        else
+        {
+            pixelB = gameObject;
+            indexB = pixelB.transform.GetSiblingIndex();
+            
+            Vector2 positionA = pixelA.transform.position;
+            pixelA.transform.position = pixelB.transform.position;
+            pixelB.transform.position = positionA;
+
+            pixelA.transform.SetSiblingIndex(indexB);
+            pixelB.transform.SetSiblingIndex(indexA);
+        }
     }
 }
