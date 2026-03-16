987ac49d8d525a5e24ce59639921ce5107861331
mocha <mocha.gamedev@gmail.com>
2026-03-16 17:43:56 -0400

    first part of hidden interaction working, just add post dialogue now

--- Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/MirrorZoom.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom1/Logic/MirrorZoom.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class MirrorZoom : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private bool postInteraction;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!postInteraction && GameProgression.GameProgressionInstance.GetFlag("firstInteractionMirrorZoom"))
+        {
+            postInteraction = true;
+            gameObject.name = "MirrorZoomPostInteraction";
+        }
     }
 }
