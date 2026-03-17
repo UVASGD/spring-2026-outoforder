bdf0f2a7ef98cde9da873e311d8264663b467e89
mocha-caramel-latte <mocha.gamedev@gmail.com>
2026-03-17 14:59:59 -0400

    mira-escaperoom0-art-2 (#39)
    * updated keyboard setup zoom
    
    * slightly better placement of lockbox colors, but needs perspective
    
    * fix placement of numbers for locked drawer secondary

--- Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/LockedDrawerPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/LockedDrawerPuzzle.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class LockedDrawerPuzzle : Puzzle
 {
+    private GameObject digits;
+    private GameObject digitsSecondary;
     private GameObject remote;
 
     void Awake()
@@ -10,7 +12,11 @@ public class LockedDrawerPuzzle : Puzzle
         answer = new char[] { '2', '3', '5', '7' };
         guess = new char[] { '0', '0', '0', '0' };
 
+        digits = transform.Find("Digits").gameObject;
+        digitsSecondary = transform.Find("DigitsSecondary").gameObject;
         remote = transform.Find("Remote").gameObject;
+
+        digitsSecondary.SetActive(false);
         remote.SetActive(false);
     }
 
@@ -18,10 +24,9 @@ public class LockedDrawerPuzzle : Puzzle
     {
         gameObject.GetComponent<Image>().sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["LockedDrawerPuzzleSecondary"];
         
-        foreach (Transform child in transform)
-        {
-            child.gameObject.SetActive(false);
-        }
+        digits.SetActive(false);
+        
+        digitsSecondary.SetActive(true);
         remote.SetActive(true);
 
         GameProgression.GameProgressionInstance.SetFlag("firstInteractionLockedDrawerPuzzle", true);
