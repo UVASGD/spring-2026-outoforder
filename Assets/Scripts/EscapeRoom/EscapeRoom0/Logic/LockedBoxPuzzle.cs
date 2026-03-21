03bcc1b7c3ec86f4353028a99d478ac5f381fdc1
mocha <mocha.gamedev@gmail.com>
2026-03-21 14:11:04 -0400

    fixed name of glowing magenta core so it can be picked up

--- Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/LockedBoxPuzzle.cs
+++ Assets/Scripts/EscapeRoom/EscapeRoom0/Logic/LockedBoxPuzzle.cs
@@ -20,7 +20,7 @@ public class LockedBoxPuzzle : Puzzle
         answer = new char[] { '0', '1', '2', '1', '0' };
         guess = new char[] { '1', '1', '1', '1', '1' };
 
-        replacementRedCore = transform.Find("ReplacementRedCore").gameObject;
+        replacementRedCore = transform.Find("GlowingMagentaCore").gameObject;
 
         GetComponentsInChildren<Image>()
             .Where(color => color.name.Contains("Color"))
