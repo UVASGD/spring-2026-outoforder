ce7ca21caa931fac6c4474d5b1c9b9891bf70e55
mocha <mocha.gamedev@gmail.com>
2026-04-18 10:43:59 -0400

    mock save and ui

---
+++ Assets/Scripts/VisualNovel/Logic/SavePopUp.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class SavePopUp : MonoBehaviour
+{
+    private GameObject confirmationPopUp;
+    private int saveToSlotNumber;
+
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        print("save is enabled");
+        if (confirmationPopUp == null)
+        {
+            confirmationPopUp = transform.Find("ConfirmationPopUp").gameObject;
+        }
+        confirmationPopUp.SetActive(false);
+    }
+
+    public void SaveToSlot(int slotNumber)
+    {
+        print($"trying to save to slot number {slotNumber}");
+        saveToSlotNumber = slotNumber;
+        confirmationPopUp.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        print("cancelled");
+        confirmationPopUp.SetActive(false);
+    }
+
+    public void Save()
+    {
+        print($"saved to {saveToSlotNumber}");
+        confirmationPopUp.SetActive(false);
+    }
+}
