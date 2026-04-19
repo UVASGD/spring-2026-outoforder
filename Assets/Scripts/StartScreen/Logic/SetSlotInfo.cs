5db682776659cd1ca33b644a40685f2f7e36a727
mocha <mocha.gamedev@gmail.com>
2026-04-18 23:04:54 -0400

    fixing UI and adding load functionality to buttons

--- Assets/Scripts/StartScreen/Logic/SetSlotInfo.cs
+++ Assets/Scripts/StartScreen/Logic/SetSlotInfo.cs
@@ -1,13 +1,13 @@
 using TMPro;
 using UnityEngine;
 
-public class SetSlotInfo : MonoBehaviour
+public class LoadMenu : MonoBehaviour
 {
     private TextMeshProUGUI slotInfo; 
 
     public void SetInfo()
     {
         slotInfo = GetComponent<TextMeshProUGUI>();
-        slotInfo.SetText("Escape Room No: " + PlayerPrefs.GetInt("Escape Room Number"));
+        // slotInfo.SetText("Escape Room No: " + PlayerPrefs.GetInt("Escape Room Number"));
     }
 }
