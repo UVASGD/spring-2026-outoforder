daf610f88ca6300f2713a5243fd2d2225dd55a0a
mocha <mocha.gamedev@gmail.com>
2026-04-26 06:30:44 -0400

    cutscene update

--- Assets/Scripts/Shared/Logic/DialogueSystemScript.cs
+++ Assets/Scripts/Shared/Logic/DialogueSystemScript.cs
@@ -363,7 +363,7 @@ public class DialogueSystem : MonoBehaviour
             {
                 speakerSpriteImageActive.sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["Transparent"];
             }
-            else
+            else if (SceneManager.GetActiveScene().name.Contains("EscapeRoom"))
             {
                 speakerSpriteAstaImageActive.sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["Transparent"];
                 speakerSpriteVirgoImageActive.sprite = GameProgression.GameProgressionInstance.SpriteCache.sprites["Transparent"];
